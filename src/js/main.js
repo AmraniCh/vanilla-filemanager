1f7c1c7def14306dea73723e2e1015d909f36e55
elamrani744 <elamrani.sv.laza@gmail.com>
2020-07-06 01:24:57 +0100

    Create the Toolbar

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,6 @@ document.querySelector('.toggle-slidebar').addEventListener("click", function ()
     document.querySelector('.slidebar').classList.toggle('show');
 });
 
+document.querySelector('.toolbar button').addEventListener("click", function () {
+    alert();
+});
