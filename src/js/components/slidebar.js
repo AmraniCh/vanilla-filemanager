7897cc5e0edb87d16046a0b25f9f2751ade83955
elamrani744 <elamrani.sv.laza@gmail.com>
2020-07-16 19:10:29 +0100

    Slidebar fix

--- src/js/components/slidebar.js
+++ src/js/components/slidebar.js
@@ -2,3 +2,17 @@
 fmWrapper.querySelector('.toggle-slidebar').addEventListener("click", function () {
     document.querySelector('.slidebar').classList.toggle('show');
 });
+
+// Close the slidebar when clicking out of it
+fmWrapper.addEventListener('click', function (e) {
+    console.log(e.target.closest('button[data-close="modal"]'));
+    if (
+        !e.target.closest('.slidebar') &&
+        !e.target.closest('.toggle-slidebar') &&
+        !e.target.closest('.settings-modal') &&
+        !e.target.closest('.modal-overlay') ||
+        e.target.closest('button[data-close="modal"]') // cancel btn
+    ) {
+        fmWrapper.querySelector('.slidebar').classList.remove('show');
+    }
+});
