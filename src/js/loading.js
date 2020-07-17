0ade3d96281aadd96c339160bc96d390e58e4978
elamrani744 <elamrani.sv.laza@gmail.com>
2020-07-17 05:50:41 +0100

    Loading remove testing timeout

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,7 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
-        fmWrapper.querySelectorAll('.loading').forEach(function (item) {
-            item.classList.remove('loading');
-        });
-    }, 500);
+    fmWrapper.querySelectorAll('.loading').forEach(function (item) {
+        item.classList.remove('loading');
+    });
 });
