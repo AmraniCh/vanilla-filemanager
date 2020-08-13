471af0afa5cc0f8eac6d1484b78bab7f65b29356
elamrani744 <elamrani.sv.laza@gmail.com>
2020-08-13 03:51:17 +0100

    Fix header search issue

--- src/js/components/header-search.js
+++ src/js/components/header-search.js
@@ -1,10 +1,12 @@
-var
-    items = fmWrapper.querySelectorAll('.files-table .file-item'),
-    searchInput = fmWrapper.querySelector('.header .search-form .search-input');
+var searchInput = fmWrapper.querySelector('.header .search-form .search-input');
+
+function getFileItems() {
+    return  fmWrapper.querySelectorAll('.files-table .file-item');
+}
 
 searchInput.addEventListener('input', function () {
     var search = this.value.toLowerCase();
-    items.forEach(function (ele) {
+    getFileItems().forEach(function (ele) {
         var name = ele.querySelector('.file-name').textContent.toLowerCase();
         if (name.indexOf(search) !== -1) {
             ele.classList.remove('hide-in-search');
