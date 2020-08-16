d55351eb5ab56598f6cc5cfb50d4761d76393a8f
elamrani744 <elamrani.sv.laza@gmail.com>
2020-08-16 04:09:01 +0100

    dist

--- dist/filemanager-template.js
+++ dist/filemanager-template.js
@@ -4,6 +4,18 @@
     // Making a reference to the fileManager wrapper dom element in the window object
     window.fmWrapper = document.querySelector('.fm-wrapper');
 
+    // Handle the enter key press when a modal is opened
+    window.addEventListener('keyup', function (e) {
+        if (e.key !== 'Enter') return;
+        var ele = fmWrapper.querySelector('.modal:not(.editor-modal).show .modal-footer button:not([data-close="modal"])');
+        if (ele) {
+            ele.dispatchEvent(new MouseEvent('click', {
+                bubbles: true,
+                cancelable: true
+            }));
+        }
+    });
+
     // polyfill for the Element.prototype.closest method
     if (!Element.prototype.closest) {
         var proto = Element.prototype;
