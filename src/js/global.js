0905c9ecdc95e7e0862a03f305af841b8c26f607
elamrani744 <elamrani.sv.laza@gmail.com>
2020-08-16 04:07:49 +0100

    Handle the enter key press

--- src/js/global.js
+++ src/js/global.js
@@ -1,2 +1,14 @@
 // Making a reference to the fileManager wrapper dom element in the window object
 window.fmWrapper = document.querySelector('.fm-wrapper');
+
+// Handle the enter key press when a modal is opened
+window.addEventListener('keyup', function (e) {
+    if (e.key !== 'Enter') return;
+    var ele = fmWrapper.querySelector('.modal:not(.editor-modal).show .modal-footer button:not([data-close="modal"])');
+    if (ele) {
+        ele.dispatchEvent(new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true
+        }));
+    }
+});
